ddf8954ffd74e6b814964d6a45e360f5d006ed26
mr.Mhoo <thanathorn@vertice-international.com>
2018-11-07 12:09:53 +0700

    add http

---
+++ project/doassignment/src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>List vue mixins</h1>
+       <input type="text" v-model="filterText">
+       <ul>
+           <li v-for="item in filterdFruits">{{item}}</li>
+       </ul>
+  </div>
+</template>
+
+<script>
+import { fruitMixin } from "../js/fruitMixin.js";
+export default {
+  mixins: [fruitMixin]
+};
+</script>
+
+<style>
+</style>
