e33aac2f6e7b04ab1915ba16ff80f8be410cfbd5
mr.Mhoo <thanathorn@vertice-international.com>
2018-11-08 16:41:39 +0700

    learnVuex

---
+++ project/start-vuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Vuex</h1>
+                <app-result></app-result>
+                <app-another-result></app-another-result>
+                <hr>
+                <app-counter></app-counter>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Counter from "./components/Counter";
+import Result from "./components/Result";
+import AnotherResult from "./components/AnotherResult";
+
+export default {
+  components: {
+    appCounter: Counter,
+    appResult: Result,
+    appAnotherResult: AnotherResult
+  }
+};
+</script>
+
---
+++ project/start-vuex/src/components/AnotherResult.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>AnotherCounter is: {{ doubleCounter }}</p>
+    <p>NUmber on Click: {{stringCounter}}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  // props: ['counter']
+  computed: {
+    ...mapGetters(["doubleCounter", "stringCounter"])
+  }
+};
+</script> 
