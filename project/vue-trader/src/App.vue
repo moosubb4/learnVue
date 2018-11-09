526eb21d6af4fc7ff72dc0468ad09b5695d4e69d
mr.Mhoo <thanathorn@vertice-international.com>
2018-11-09 18:02:18 +0700

    add headers

--- project/vue-trader/src/App.vue
+++ project/vue-trader/src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="container">
+    <app-header></app-header>
+
+    <div class="columns">
+      <div class="column">
+         <router-view></router-view>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/shared/Header";
+export default {
+  components: {
+    appHeader: Header
+  }
+};
 </script>
 
 <style>
--- project/vue-trader/src/components/shared/Header.vue
+++ project/vue-trader/src/components/shared/Header.vue
@@ -1,11 +1,76 @@
 <template>
   <div>
-    <h3>Header Component</h3>
+<nav class="navbar is-info" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <router-link 
+     to="/"
+     tag="a"
+     class="navbar-item">
+      <b>STOCK TRADER</b>
+    </router-link>
+
+    <a role="button" class="navbar-burger burger" 
+       aria-label="menu" 
+       aria-expanded="false" 
+       data-target="navbarBasicExample"
+       @click="onHamburger = !onHamburger">
+
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" 
+       :class="{'is-active':onHamburger}" class="navbar-menu">
+
+    <div class="navbar-start">
+      <router-link tag="a" to="/portfolio" 
+      active-class="is-active" exact
+      class="navbar-item">
+        Portfolio
+      </router-link>
+
+      <router-link tag="a" to="/stocks"
+      active-class="is-active" exact
+      class="navbar-item">
+        Stocks
+      </router-link>
+    </div>
+
+    <div class="navbar-end"> 
+        <a class="navbar-link">
+            End Day
+        </a>
+      <div class="navbar-item is-hoverable">
+        <a class="navbar-link">
+            Save&Load
+        </a>
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+             Save Data
+          </a>
+          <a class="navbar-item">
+             Load Data
+          </a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</nav>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      onHamburger: false
+    };
+  },
+  methods: {}
+};
 </script>
 
 <style>
